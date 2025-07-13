ffb9b04cf6353408634fe4f8d3e92b34bd8dc403
slim <slimabida21@gmail.com>
2025-07-13 20:25:15 +0200

    mise en place du formulaire dans form.js et gestion des erreurs

--- src/form/form.js
+++ src/form/form.js
@@ -1,6 +1,33 @@
-import './form.scss';
-import '../assets/styles/styles.scss';
+import "./form.scss";
+import "../assets/styles/styles.scss";
 
-
-
-
+// Mise en place du formulaire en javascript //
+const form = document.querySelector("form");
+let errors = [];
+const errorElement = document.querySelector("#errors");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const article = Object.fromEntries(formData.entries());
+  // gérer les erreurs avant l'envoie vers le serveur //
+  if (formIsValid(article)) {
+    const json = JSON.stringify(article);
+    // si le formulaire est valide on va faire le fetch //
+  }
+});
+const formIsValid = (article) => {
+  if (!article.author || !article.category || !article.content) {
+    errors.push("Vous devez renseigner tous les champs.");
+  } else {
+    errors = [];
+  }
+  if (errors.length) {
+    let errorHTML = "";
+    errors.forEach((e) => {
+      errorHTML += `<li>${e}</li>`;
+    });
+    errorElement.innerHTML = errorHTML;
+  } else {
+    errorElement.innerHTML = ''; 
+  }
+};
