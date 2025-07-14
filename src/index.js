6a74400a0473d4a6dcd9d5fbc708c71d23f2eb0f
slim <slimabida21@gmail.com>
2025-07-14 15:16:51 +0200

    Mise en place de la supression d'articles test OK

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const createArticles = (articles) => {
             <p class="article-content">${article.content}</p>
             <div class="article-action">
               
-              <button class="btn btn-danger" data-id=${article.id} >Supprimer</button>
+              <button class="btn btn-danger" data-id="${article._id}" >Supprimer</button>
             </div>
          
           `;
@@ -30,6 +30,29 @@ const createArticles = (articles) => {
   });
   articleContainerElement.innerHTML = "";
   articleContainerElement.append(...articlesDOM);
+
+  // === SUPPRESSION DES ARTICLES  ===
+
+  const deleteButtons = articleContainerElement.querySelectorAll(".btn-danger");
+  console.log(deleteButtons);
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", async (event) => {
+      try {
+        const target = event.target;
+        const articleId = target.dataset.id;
+        const response = await fetch(
+          `https://restapi.fr/api/article/${articleId}`,
+          {
+            method: "DELETE",
+          }
+        );
+        const body = await response.json();
+        fetchArticle();
+      } catch (error) {
+        console.log("Erreur : ", error);
+      }
+    });
+  });
 };
 
 // === FONCTION POUR METTRE À JOUR UN ARTICLE (À utiliser manuellement) ===
@@ -54,29 +77,31 @@ const updateArticle = async (articleId, updatedData) => {
   }
 };
 
-//Mise a jour insertion de photo  article 1// 
+//Mise a jour insertion de photo  article 1//
 updateArticle("6874b87c8dae9bcf6cddd2d5", {
   author: "Nexa Digital school",
   category: "Technologie",
   content: "La première chose à savoir est ce...",
   title: "Quelles sont les bases du langage JavaScript",
-  img: "https://randomuser.me/api/portraits/men/25.jpg"
+  img: "https://randomuser.me/api/portraits/men/25.jpg",
 });
-//Mise a jour ajout de photo dans l'article 2// 
+//Mise a jour ajout de photo dans l'article 2//
 updateArticle("6874bb028dae9bcf6cddd2d6", {
   author: "Arthur Weill ",
   category: "Technologie",
-  content: "Cette première boucle va fonctionner avec un « compteur » qui va nous permettre de définir précisément le nombre d’itération à réaliser, ainsi que l’évolution de ce « compteur ».",
+  content:
+    "Cette première boucle va fonctionner avec un « compteur » qui va nous permettre de définir précisément le nombre d’itération à réaliser, ainsi que l’évolution de ce « compteur ».",
   title: "La boucle for en PHP",
-  img: "https://randomuser.me/api/portraits/men/26.jpg"
+  img: "https://randomuser.me/api/portraits/men/26.jpg",
 });
-// Mise a joour de photo dans l'article 3 // 
+// Mise a joour de photo dans l'article 3 //
 updateArticle("6874bb408dae9bcf6cddd2d7", {
   author: "Arthur Weill ",
   category: "Technologie",
-  content: "Cette première boucle va fonctionner avec un « compteur » qui va nous permettre de définir précisément le nombre d’itération à réaliser, ainsi que l’évolution de ce « compteur ».",
+  content:
+    "Cette première boucle va fonctionner avec un « compteur » qui va nous permettre de définir précisément le nombre d’itération à réaliser, ainsi que l’évolution de ce « compteur ».",
   title: "La boucle While en PHP ",
-  img: "https://randomuser.me/api/portraits/men/27.jpg"
+  img: "https://randomuser.me/api/portraits/men/27.jpg",
 });
 
 // === FONCTION POUR RÉCUPÉRER LES ARTICLES DEPUIS L'API ===
